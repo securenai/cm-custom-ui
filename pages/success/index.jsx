181fd02385ac63deff83030ee90ade29e5b1461e
Michael Cheng <michaelnccu@gmail.com>
2023-01-09 10:40:15 +0800

    Update index.jsx

--- pages/success/index.jsx
+++ pages/success/index.jsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.css';
+import { useEffect } from 'react';
 // import style from './style.css';
 
 export default function Success() {
+  useEffect(() => {
+    confirm('Open Master Control + Agent?');
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
